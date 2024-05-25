bce57fa5cbfb4558ee33f60ec9aa979d76679eac
itsuki <itsuki.enjyoy@gmail.com>
2024-05-25 10:40:13 +0900

    Initial Commit

---
+++ NetworkLayerDemo/NetworkLayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayerDemoApp.swift
+//  NetworkLayerDemo
+//
+//  Created by Itsuki on 2024/05/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
